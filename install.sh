34406df4093dd3abdb6efab2934273cc19185573
lcybrgl <cybrg@inbox.ru>
2017-11-26 23:41:24 +0300

    Refactoring a little

--- install.sh
+++ install.sh
@@ -6,7 +6,18 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 bld=`tput bold`
 fmt="%-12s%-12s\n"
-backdir=~/dotfiles-`date +%Y%m%d`
+backdir=~/dotfiles-bak
+
+function show_usage()
+{
+    echo -e "\nUsage: install.sh [PARAMETER]...\nScript for installing my default dotfiles\nAnd Powerline fonts\n"
+    echo -e "all\t--Install fonts, dotfiles, vim plugins"
+    echo -e "dfiles\t--Install only dotfiles"
+    echo -e "vim\t--Install only vim plugins"
+    echo -e "fonts\t--Install fonts to local user dir"
+    echo ""
+    exit 1
+}
 
 function program_is_installed()
 {
@@ -25,22 +36,24 @@ function program_is_installed()
     echo ""
 }
 
-function show_usage()
-{
-    echo -e "\nUsage: install.sh [PARAMETER]...\nScript for installing my default dotfiles\nAnd Powerline fonts\n"
-    echo -e "check\t--Check if needed tools are installed"
-    echo -e "console\t--Install vim-plugins, dotfiles and fonts"
-    echo ""
-    exit 1
-}
-
 function syncf()
 {
+    if [ ! -d "$backdir" ]
+    then
+        mkdir $backdir
+    fi
+
     for i in "$@"
     do
         if [ -f ~/.$i ]
         then
-            cp -a ~/.$i $backdir/.$i.bak
+            local count=`ls -l $backdir| grep $i | wc -l`
+            if [ "$count" == 0 ]
+            then
+                cp -a ~/.$i $backdir/$i-`date +%Y%m%d`
+            else
+                cp -a ~/.$i $backdir/$i-`date +%Y%m%d`-${count}
+            fi
             rsync dotfiles/$i ~/.$i 1>/dev/null
         else
             rsync dotfiles/$i ~/.$i 1>/dev/null
@@ -52,27 +65,40 @@ function syncf()
 [ ! -n "$1" ] && show_usage
 
 trap 'echo ""; echo "Exiting script..."; exit 1;' SIGINT SIGQUIT SIGTSTP
+program_is_installed rsync fc-cache vim screen
+
 while [ -n "$1" ]
 do
 case $1 in
-"check" )
-    echo -e "Just checking"
-    program_is_installed rsync fc-cache vim screen
-    ;;
-"console" )
-    program_is_installed rsync fc-cache vim screen
-    echo "Installing fonts..."
+"all" )
+    echo -n "Installing fonts..."
     rsync -arv fonts/* ~/.fonts/ 1>/dev/null
     fc-cache -f ~/.fonts 1>/dev/null
-    echo "Installing vim-plugins..."
+    echo " ${bld}${green}+${reset}"
+    echo -n "Installing vim-plugins..."
     rsync -arv vim-plugins/bundle ~/.vim/ --delete 1>/dev/null
-    echo "Installing dotfiles..."
-    if [ ! -d "$backdir" ]
-    then
-        mkdir $backdir
-    fi
+    echo " ${bld}${green}+${reset}"
+    echo -n "Installing dotfiles..."
     syncf bashrc vimrc screenrc Xresources
     xrdb -merge ~/.Xresources
+    echo " ${bld}${green}+${reset}"
+    ;;
+"dfiles" )
+    echo -n "Installing dotfiles..."
+    syncf bashrc vimrc screenrc Xresources
+    xrdb -merge ~/.Xresources
+    echo " ${bld}${green}+${reset}"
+    ;;
+"fonts" )
+    echo -n "Installing fonts..."
+    rsync -arv fonts/* ~/.fonts/ 1>/dev/null
+    fc-cache -f ~/.fonts 1>/dev/null
+    echo " ${bld}${green}+${reset}"
+    ;;
+"vim" )
+    echo -n "Installing vim-plugins..."
+    rsync -arv vim-plugins/bundle ~/.vim/ --delete 1>/dev/null
+    echo " ${bld}${green}+${reset}"
     ;;
 * )
     show_usage
